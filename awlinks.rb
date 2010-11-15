afa72c9d4a89a85739d185b5ede1d98b44f89fb1
Austin Wise <AustinWise@gmail.com>
2010-11-14 21:04:22 -0800

    Fixed xml file name.

--- awlinks.rb
+++ awlinks.rb
@@ -4,7 +4,7 @@ require "bundler/setup"
 require 'sinatra'
 require 'xml'
 
-doc = XML::Document.file('Gems.xml')
+doc = XML::Document.file('Links.xml')
 links = []
 doc.find('//links/link').each do |g|
   links << g.attributes.inject({}) { |h, a| h[a.name] = a.value; h }
